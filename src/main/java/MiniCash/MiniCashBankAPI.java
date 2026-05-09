792870de57a6b1787fa2ee6a32ba7491c939cc7c
MiniCash <tminecraft2143@gmail.com>
2026-05-09 21:26:11 +0900

    Update MiniCashBankAPI.java

--- src/main/java/MiniCash/MiniCashBankAPI.java
+++ src/main/java/MiniCash/MiniCashBankAPI.java
@@ -8,7 +8,7 @@ import java.util.concurrent.CompletableFuture;
 
 public interface MiniCashBankAPI {
 
-    CompletableFuture<BankAPIResult<PlayerData>> has(UUID uuid);
+    CompletableFuture<BankAPIResult<Boolean>> has(UUID uuid);
 
     CompletableFuture<BankAPIResult<PlayerData>> addTable(OfflinePlayer player,String user,String pluginName);
 
@@ -28,9 +28,16 @@ public interface MiniCashBankAPI {
     CompletableFuture<BankAPIResult<PlayerData>> deposit(OfflinePlayer player, String user, int amount, String pluginName);
     CompletableFuture<BankAPIResult<PlayerData>> withdraw(OfflinePlayer player, String user, int amount, String pluginName);
 
+    // editの簡略化版
+    // spendメソッドは電子マネーからただ引き出すwithdraw
+    // debitメソッドは銀行からただ引き出すwithdraw
+    CompletableFuture<BankAPIResult<PlayerData>> spend(OfflinePlayer player, String user, int amount, String pluginName);
+    CompletableFuture<BankAPIResult<PlayerData>> debit(OfflinePlayer player, String user, int amount, String pluginName);
+
 
 
     CompletableFuture<BankAPIResult<PayResponseDTO>> pay(UUID fromUUID,UUID toUUID,String user, int amount, String pluginName);
+
 
 
 
