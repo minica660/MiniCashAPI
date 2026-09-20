eca2af3b1d8e7be41f80e5e5bed6206b45a486bb
MiniCash <tminecraft2143@gmail.com>
2026-09-20 20:46:03 +0900

    セミコロン抜けの修正

--- src/main/java/MiniCash/MiniCashBankAPI.java
+++ src/main/java/MiniCash/MiniCashBankAPI.java
@@ -29,7 +29,7 @@ public interface MiniCashBankAPI {
     CompletableFuture<BankAPIResult<Integer>> getEmoney(UUID uuid);
     CompletableFuture<BankAPIResult<Integer>> getBank(UUID uuid);
     CompletableFuture<BankAPIResult<Integer>> getPoint(UUID uuid);
-    CompletableFuture<BankAPIResult<MoneyLogPageResponseDTO>> getPlayerLogs(UUID playerUUID, int page, int size)
+    CompletableFuture<BankAPIResult<MoneyLogPageResponseDTO>> getPlayerLogs(UUID playerUUID, int page, int size);
     CompletableFuture<BankAPIResult<PlayerData>> getLog(UUID uuid,String user,String pluginName);
 
     CompletableFuture<BankAPIResult<PlayerData>> getStatus(UUID uuid,String user,String pluginName);
